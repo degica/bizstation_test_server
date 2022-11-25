0c9603ccd9f96c511c14bcfe657eec1dfabe0b0c
Metallion <akieckens@degica.com>
2022-11-25 13:39:44 +0900

    Generated a gem with bundler

---
+++ lib/bizstation/test/server.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "server/version"
+
+module Bizstation
+  module Test
+    module Server
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
