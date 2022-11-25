d4e1ad900495636da69c4cb8f8841c7777c500fd
Metallion <akieckens@degica.com>
2022-11-25 14:43:14 +0900

    A little refactoring on the spec

--- spec/lib/bizstation_test_server_spec.rb
+++ spec/lib/bizstation_test_server_spec.rb
@@ -1,10 +1,8 @@
+def app
+  BizstationTestServer::Server
+end
+
 RSpec.describe BizstationTestServer::Server do
-  include Rack::Test::Methods
-
-  def app
-    BizstationTestServer::Server
-  end
-
   it "says hello" do
     get '/'
     expect(last_response).to be_ok
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,9 +3,12 @@
 require 'rspec'
 require 'rack/test'
 require_relative '../lib/bizstation_test_server'
-require_relative 'lib/bizstation_test_server_spec'
+
+Dir['lib/*_spec.rb'].each { |f| require_relative f }
 
 RSpec.configure do |config|
+  config.include Rack::Test::Methods
+
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
 
